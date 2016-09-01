741f4c0567987967b74014c5115b9604490ce771
BradWBeer <BradWBeer@gmail.com>
2016-09-01 20:45:17 +0000

    Added a library with version to the list.

--- library.lisp
+++ library.lisp
@@ -2,7 +2,7 @@
 
 (cffi:define-foreign-library :libFreeImage
   (cffi-features:darwin "libpango.dylib")
-  (cffi-features:unix (:or "libfreeimage.so"))
+  (cffi-features:unix (:or "libfreeimage.so" "libfreeimage.so.3"))
   (cffi-features:windows "libfreeimage.dll"))
 
 (cffi:load-foreign-library :libFreeImage)
